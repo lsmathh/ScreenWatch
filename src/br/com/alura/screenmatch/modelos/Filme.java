17d507f47d641bb49b2c7637b50d541521977423
lsmathh <matheus200125@live.com>
2023-11-28 15:43:24 -0300

    Commit teste

--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -7,7 +7,6 @@ public class Filme extends Titulo implements Classificavel {
 
     public Filme(String nome, int anoDeLancamento) {
         super(nome, anoDeLancamento);
-       
     }
 
     public String getDiretor() {
